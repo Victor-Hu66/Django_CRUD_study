1537c99d17be132561ea44d1f35ded08fc983f82
Ahmet <scienceworker34@gmail.com>
2021-01-29 20:28:24 +0300

    add student delete page

--- src/crudapp/urls.py
+++ src/crudapp/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import home_view, student_add, student_list, student_detail
+from .views import home_view, student_add, student_list, student_detail, student_delete
 
 
 urlpatterns = [
     path("", home_view, name="Home"),
     path("add/", student_add, name="add"),
     path("list/", student_list, name="list"),
-    path("<int:id>", student_detail, name="detail")
+    path("<int:id>", student_detail, name="detail"),
+    path("<int:id>/delete", student_delete, name="delete")
 ]
